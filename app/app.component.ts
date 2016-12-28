97d0e856bfcb48269aad20d3286d6688eb69c5f8
Nisha Kripalani <nisha@healthreveal.com>
2016-12-28 15:33:05 -0500

    making code added npm install for webdriver-manager in travis.yml

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,4 +4,4 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Rules Editor App Angular2'; }
+export class AppComponent  { name = 'Angular'; }
